a1ca70c3cfeb1735689b444e4ab69d7a670efb8e
Molham <dr.molham93@gmail.com>
2021-06-23 21:58:34 +0200

    rebuilding: the metrics route

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -16,7 +16,12 @@ def status():
 
 @app.route("/metrics")
 def metrics():
-    return "data: {UserCount: 140, UserCountActive: 23}"
+    response = app.response_class(
+        response=json.dumps({"status": "success", "code": 0, "data": {"UserCount": 140, "UserCountActive": 23}}),
+        status=200,
+        mimetype="application/json"
+    )
+    return response
 
 
 @app.route("/")
