db6fcfd3538e4a78148df8792761be7d8980e39a
Molham <dr.molham93@gmail.com>
2021-06-23 21:55:13 +0200

    rebuilding: the status route

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from flask import json
 
 app = Flask(__name__)
 
 
 @app.route("/status")
 def status():
-    return "result: OK - healthy"
+    response = app.response_class(
+        response=json.dumps({"result": "OK - healthy"}),
+        status=200,
+        mimetype="application/json"
+    )
+    return response
 
 
 @app.route("/metrics")
