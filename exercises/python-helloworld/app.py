889e869797cb5bc8590549a1acec2154dc1da4fa
Molham <dr.molham93@gmail.com>
2021-06-23 21:49:25 +0200

    reformat file: sorting the main route of the application to the end of the file

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -3,11 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!<br>this is Molham"
-
-
 @app.route("/status")
 def status():
     return "result: OK - healthy"
@@ -16,6 +11,11 @@ def status():
 @app.route("/metrics")
 def metrics():
     return "data: {UserCount: 140, UserCountActive: 23}"
+
+
+@app.route("/")
+def hello():
+    return "Hello World!<br>this is Molham"
 
 
 if __name__ == "__main__":
